97da11155d11159fe373e69bdff8e99ae1af3c31
agholson <87455899+agholson@users.noreply.github.com>
2021-11-22 18:19:27 -0500

    Initial Commit

---
+++ Firebase Demo/Firebase_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_DemoApp.swift
+//  Firebase Demo
+//
+//  Created by Shepherd on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
